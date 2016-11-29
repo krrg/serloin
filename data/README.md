6b27c7de3d1e4c2b15eafece593332bdab420f1f
Ken Reese <krrgithub@gmail.com>
2016-11-28 22:41:45 -0700

    Added data output files

---
+++ data/README.md
@@ -0,0 +1,3 @@
+# Notice
+The data contained in the `data` folder was gathered through the Stack Exchange API and is
+distributed under a (Creative Commons License)[https://creativecommons.org/licenses/by-sa/3.0/].
--- serloin-server/lib/stackexchange/stackapi.rb
+++ serloin-server/lib/stackexchange/stackapi.rb
@@ -1,4 +1,6 @@
 require "httparty"
+require "glutton_ratelimit"
+
 
 class StackExchangeApi
 
@@ -15,12 +17,44 @@ class StackExchangeApi
 
 end
 
+
+class ThrottledHTTP
+  extend GluttonRatelimit
+
+  def initialize
+    @backoff = nil
+  end
+
+  def get_json(uri, query)
+    unless @backoff.nil?
+      sleep(@backoff)
+      @backoff = nil
+    end
+
+    response = HTTParty.get(uri, query)
+    unless response.code == 200
+      return {"items": []}
+    end
+
+    result = JSON.parse(response.body)
+
+    if result.key?("backoff")
+      @backoff = result["backoff"]
+    end
+
+    result
+  end
+
+  rate_limit :get_json, 60, 60  # 1 GET request every 1 second
+end
+
 class StackExchangeRequestBuilder
 
   def initialize(base_uri='api.stackexchange.com', version='2.2', site='stackoverflow', api_key=nil)
     @base_uri = base_uri
     @version = version
     @site = site
+    @http = ThrottledHTTP.new
   end
 
   def build_request(path, query_params={})
@@ -30,7 +64,7 @@ class StackExchangeRequestBuilder
     )
   end
 
-
+  # TODO: Make this more robust against bad number of pages
   """
   Warning: This method blithely assumes that the number of pages specified will actually
   be available.  It will explode if this is false.  So, be careful with how many pages
@@ -45,8 +79,8 @@ class StackExchangeRequestBuilder
     }
 
     responses = (1..pages).map do |page|
-      build_request("/tags", query_params.merge({"page" => page})) do |uri, query|
-        JSON.parse(HTTParty.get(uri, :query => query).body)
+      build_request "/tags", query_params.merge({"page" => page}) do |uri, query|
+        @http.get_json(uri, :query => query)
       end
     end
 
@@ -60,10 +94,22 @@ class StackExchangeRequestBuilder
     path = "/tags/#{tag}/top-answerers/all_time"
     query_params = {
       "pagesize": 100,
+      "filter": "!-pcLI4I7",
     }
+
+    build_request path, query_params do |uri, query|
+      @http.get_json(uri, :query => query)
+    end
   end
 
 end
 
 S = StackExchangeRequestBuilder.new
-puts(S.tags(pages=3))
+
+S.tags(pages=4).map do |tag|
+  File.open("../../../data/#{tag}.json", "w") do |file|
+    puts "Getting top answerers for tag `#{tag}`"
+    response = S.top_answerers_for_tag(tag)
+    file.write(response.to_json)
+  end
+end
