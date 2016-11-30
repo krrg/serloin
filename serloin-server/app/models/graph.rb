e4a37871dee6383825f859174318bb6525323b04
tjmonson <princenosnow@gmail.com>
2016-11-30 15:39:13 -0700

    Add add_edge function to Graph ActiveRecord. Call using Graph.add_edge(params)

--- serloin-server/app/models/graph.rb
+++ serloin-server/app/models/graph.rb
@@ -1,2 +1,15 @@
 class Graph < ActiveRecord::Base
+  validates :src_tag, presence: true, length: { maximum: 100 }
+  validates :dest_tag, presence: true, length: { maximum: 100 }
+  validates :upvotes, numericality: { greater_than_or_equal_to: 0 }
+
+  def Graph.add_edge(src_tag, dest_tag, upvotes)
+    edge = find_by(src_tag: src_tag, dest_tag: dest_tag)
+    if edge
+        edge.update(upvotes: edge.upvotes + upvotes)
+    else
+      create(src_tag: src_tag, dest_tag: dest_tag, upvotes: upvotes)
+    end
+  end
+
 end
