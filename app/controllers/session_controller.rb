1be27499f3ebedf87962b13aa78bb1ae7416373f
tjmonson <princenosnow@gmail.com>
2016-12-12 16:46:47 -0700

    Debugging statement

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -25,7 +25,8 @@ class SessionController < ApplicationController
       flash[:danger] = "There was an error authenticating with Stack Exchange"
       redirect_to root_url
     end
-    session[:access_token] = response.code.split('=')[1]
+
+    session[:access_token] = @response.code.split('=')[1]
     @access_token = session[:access_token]
   end
 
