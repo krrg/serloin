693341795d1466907db67b14d5e1a0c7a4506171
tjmonson <princenosnow@gmail.com>
2016-12-12 12:08:52 -0700

    More tweeks

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :stackoverflow, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], :scope => 'no_expiry', :key => ENV['CLIENT_KEY']
-end
