7510210ccfaba36dae091647654092836d15b10b
tjmonson <princenosnow@gmail.com>
2016-12-12 12:24:37 -0700

    More on oauth flow

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,5 +1,18 @@
+require 'cgi'
+require 'uri'
+
 class SessionController < ApplicationController
   def create
 
+    uri = URI.parse(request.url)
+    params = CGI.parse(uri.query)
+    uri = URI.parse('https://stackexchange.com')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    post_requ = Net::HTTP::Post.new("/oauth/access_token")
+    post_requ.add_field('Content-Type', 'application/x-www-form-urlencoded')
+    post_requ.set_form_data(params)
+    response = http.request(request)
   end
 end
