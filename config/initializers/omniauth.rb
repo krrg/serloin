c5fa9478cbfafd0543544d2455a5ae59705e4dd7
tjmonson <princenosnow@gmail.com>
2016-12-12 11:19:49 -0700

    Oauth parameter name change

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :stackoverflow, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], :scope => 'no_expiry', :oauth_key => ENV['CLIENT_KEY']
+  provider :stackoverflow, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], :scope => 'no_expiry', :key => ENV['CLIENT_KEY']
 end
