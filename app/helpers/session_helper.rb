4bab5079246b68eb20d4cf0066e657439b64ddfb
tjmonson <princenosnow@gmail.com>
2016-12-13 13:37:12 -0700

    Add sorted question link details

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,2 +1,9 @@
 module SessionHelper
+  def find_question(recent_question, id)
+    recent_questions.each do |question|
+      if question.questionId == id
+        return question
+      end
+    end
+  end
 end
